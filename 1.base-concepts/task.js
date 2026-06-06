a687baa9dbd31b3e135c0e019b62888e0895e517
sack-of-pigs <jtk.dethparty@gmail.com>
2026-06-06 13:27:27 +0400

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,20 +18,32 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   if (typeof percent === 'string') {
     p = Number(percent);
   }
+  if (isNaN(p)) { 
+    return false;
+  }
 
   let c = contribution;
   if (typeof contribution === 'string') {
     c = Number(contribution);
+  }
+  if (isNaN(c)) { 
+    return false;
   }
 
   let a = amount;
   if (typeof amount === 'string') {
     a = Number(amount);
   }
+  if (isNaN(a)) { 
+    return false;
+  }
 
   let n = countMonths;
   if (typeof countMonths === 'string') {
     n = Number(countMonths);
+  }  
+  if (isNaN(n)) { 
+    return false;
   }
 
   if (a <= c) {
@@ -40,7 +52,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
   const monthlyRate = (p / 100) / 12;
   const loanBody = a - c;
-  const monthlyPayment = loanBody * (monthlyRate + (monthlyRate / ((1 + monthlyRate) ** n - 1)));
+  const monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (((1 + monthlyRate) ** n) - 1)));
   const totalPayment = c + monthlyPayment * n;
   return Math.round(totalPayment * 100) / 100;
 }
