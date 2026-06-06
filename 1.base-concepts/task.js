4c298081f34b8a37818e4cf96663ad375d0eaeb4
sack-of-pigs <jtk.dethparty@gmail.com>
2026-06-06 12:55:42 +0400

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -14,33 +14,33 @@ function solveEquation(a, b, c) {
   }
 }
 
-//function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-//  let p = percent;
-//  if (typeof percent === 'string') {
-//    p = Number(percent);
-//  }
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let p = percent;
+  if (typeof percent === 'string') {
+    p = Number(percent);
+  }
 
-//  let c = contribution;
-//  if (typeof contribution === 'string') {
-//    c = Number(contribution);
-//  }
+  let c = contribution;
+  if (typeof contribution === 'string') {
+    c = Number(contribution);
+  }
 
-//  let a = amount;
-//  if (typeof amount === 'string') {
-//    a = Number(amount);
-//  }
+  let a = amount;
+  if (typeof amount === 'string') {
+    a = Number(amount);
+  }
 
-//  let n = countMonths;
-//  if (typeof countMonths === 'string') {
-//    n = Number(countMonths);
-//  }
-//  if (a <= c) {
-//    return 0;
-//  }
-//  const monthlyRate = (p / 100) / 12;
-//  const loanBody = a - c;
-//  const monthlyPayment = loanBody * (monthlyRate + (monthlyRate / ((1 + monthlyRate) ** n - 1)));
-//  const totalPayment = c + monthlyPayment * n;
+  let n = countMonths;
+  if (typeof countMonths === 'string') {
+    n = Number(countMonths);
+  }
+  if (a <= c) {
+    return 0;
+  }
+  const monthlyRate = (p / 100) / 12;
+  const loanBody = a - c;
+  const monthlyPayment = loanBody * (monthlyRate + (monthlyRate / ((1 + monthlyRate) ** n - 1)));
+  const totalPayment = c + monthlyPayment * n;
   
-//  return Math.round(totalPayment * 100) / 100;
-//}
+  return Math.round(totalPayment * 100) / 100;
+}
