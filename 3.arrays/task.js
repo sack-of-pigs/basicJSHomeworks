578174f7da66237aeaad7adb050f955bd99b569b
sack-of-pigs <jtk.dethparty@gmail.com>
2026-06-11 17:13:36 +0400

    Update task.js
    Update 3.arrays\task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(user => user.gender === gender).reduce((sum, user, index, arr) => {
+        sum += user.age;
+        return index === arr.length - 1 ?  sum / arr.length : sum;
+    }, 0);
 }
