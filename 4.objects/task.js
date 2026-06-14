a087a6bc9cf2dfbfc6c2a83403e8be9e2c27f131
sack-of-pigs <jtk.dethparty@gmail.com>
2026-06-14 14:23:37 +0400

    Update 4.objects\task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.marks) {
+        return;
+    }
+    this.marks.push(...marksToAdd);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    return this.marks.reduce((sum, mark, index, arr) => {
+        sum += mark;
+        return index === arr.length - 1 ?  sum / arr.length : sum;
+    }, 0);
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
