dcff48695cb5f5a9d0e884066879b631328d8eac
sack-of-pigs <jtk.dethparty@gmail.com>
2026-06-28 13:38:10 +0400

    Update 6.exception-closure\task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿//Задание 1.
+function parseCount(value) {
+  const result = Number.parseFloat(value);
+  if (Number.isNaN(result)) {
+    throw new Error('Невалидное значение');
+  }
+  return result;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+//Задание 2.
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+  get area() {
+    const p = this.perimeter / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return Number(area.toFixed(3));
+  }
+}
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      }
+    };
+  }
+}
